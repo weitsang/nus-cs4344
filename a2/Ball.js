18b6606d730be6acbd7c83929c3b9c31549392f3
Ooi Wei Tsang <weitsang@yahoo.com>
2015-01-26 06:47:18 +0800

    Retab, make vx/vy public, add velocity update flag

--- a2/Ball.js
+++ a2/Ball.js
@@ -16,114 +16,121 @@
 "use strict"; 
 
 function Ball() {
-    // Private variables
-	var moving;	// boolean of whether ball is moving
-	var lastUpdate; // timestamp of lastUpdate
+        // Private variables
+    var moving; // boolean of whether ball is moving
+    var lastUpdate; // timestamp of lastUpdate
 
-	// Public variables
-	this.x;		// x-coordinate of ball's position 
-	this.y;		// y-coordinate of ball's position
-	this.vx;	// x-component of ball's velocity
-	this.vy;	// y-component of ball's velocity
+    // Public variables
+    this.x;     // x-coordinate of ball's position 
+    this.y;     // y-coordinate of ball's position
+    this.vx;    // x-component of ball's velocity
+    this.vy;    // y-component of ball's 
+        this.velocityUpdated; // has the ball bounced/stopped?
 
-	// constructor
-	var that = this;
-	moving = false;
-	vx = 0;
-	vy = 0;
-	this.x = Pong.WIDTH/2;
-	this.y = Pong.HEIGHT/2;
+    // constructor
+    var that = this;
+    moving = false;
+    this.vx = 0;
+    this.vy = 0;
+    this.x = Pong.WIDTH/2;
+    this.y = Pong.HEIGHT/2;
+        this.velocityUpdated = false;
 
     /*
      * private method: updateVelocity(px)
      *
-	 * Called from moveOneStep whenever the ball hits the 
-	 * or paddle.  px is the x-position of the paddle.
+     * Called from moveOneStep whenever the ball hits the 
+     * or paddle.  px is the x-position of the paddle.
      * 
      * The velocity changes depending on which region the ball
      * hits the paddle.
      */
-	var updateVelocity = function(px) {
-		// Change direction (vx) depending on collision point between ball and paddle
-		if (that.x >= px - Paddle.R1 && that.x <= px + Paddle.R1) {
-            vy = -vy;
+    var updateVelocity = function(px) {
+        // Change direction (vx) depending on collision point between ball and paddle
+        that.velocityUpdated = true;
+        if (that.x >= px - Paddle.R1 && that.x <= px + Paddle.R1) {
+            that.vy = -that.vy;
         } else if (that.x >= px - Paddle.R2 && that.x <= px + Paddle.R2) {
-            vx += (that.x > px? 1 : -1);
-            vy = -vy;
+            that.vx += (that.x > px? 1 : -1);
+            that.vy = -that.vy;
         } else if (that.x >= px - Paddle.R3 && that.x <= px + Paddle.R3) {
-            vx += (that.x > px? 2 : -2);
-            vy = -vy;
+            that.vx += (that.x > px? 2 : -2);
+            that.vy = -that.vy;
         } else if (that.x + Ball.WIDTH/2 >= px - Paddle.WIDTH/2 && that.x - Ball.WIDTH/2 <= px + Paddle.WIDTH/2) {
-            vx += (that.x > px? 3 : -3);
-            vy = -vy;
+            that.vx += (that.x > px? 3 : -3);
+            that.vy = -that.vy;
+        } else {
+            // ball didn't collide with paddle
+            that.velocityUpdated = false;
         }
-        // else = ball didn't collide with paddle
-	}
+    }
 
     /*
      * priviledged method: startMoving()
      *
-	 * Change the ball velocity to non-zero and 
-	 * set the moving flag to true.
+     * Change the ball velocity to non-zero and 
+     * set the moving flag to true.
      */
-	this.startMoving = function(){
-		vx = 0;
-		vy = Ball.VERTICAL_VELOCITY;
-		moving = true;
-		lastUpdate = getTimestamp();
-	}
-	  
+    this.startMoving = function(){
+        that.vx = 0;
+        that.vy = Ball.VERTICAL_VELOCITY;
+        moving = true;
+        lastUpdate = getTimestamp();
+    }
+      
     /*
      * priviledged method: isMoving()
      *
-	 * Return true iff the ball is moving.
+     * Return true iff the ball is moving.
      */
-	this.isMoving = function() {
-		return moving;
-	}
+    this.isMoving = function() {
+        return moving;
+    }
 
 
     /*
      * priviledged method: moveOneStep(topPaddle, bottomPaddle)
      *
-	 * Called to calculate the new position of the ball.  Update 
-	 * velocity if the ball hits the paddle.
+     * Called to calculate the new position of the ball.  Update 
+     * velocity if the ball hits the paddle.
      */
-	this.moveOneStep = function(topPaddle, bottomPaddle) {
-		var now = getTimestamp(); // get the current time in millisecond resolution
-		
-		// Update position
-		that.x += vx*(now - lastUpdate)*Pong.FRAME_RATE/1000;
-		that.y += vy*(now - lastUpdate)*Pong.FRAME_RATE/1000;
+    this.moveOneStep = function(topPaddle, bottomPaddle) {
+        var now = getTimestamp(); // get the current time in millisecond resolution
+        
+        // Update position
+        that.x += that.vx*(now - lastUpdate)*Pong.FRAME_RATE/1000;
+        that.y += that.vy*(now - lastUpdate)*Pong.FRAME_RATE/1000;
 
-		lastUpdate = now;
+        lastUpdate = now;
 
-		// Check for bouncing
-		if (that.x <= Ball.WIDTH/2 || that.x >= Pong.WIDTH - Ball.WIDTH/2) {
-			// Bounds off horizontally
-			vx = -vx;
-		} else if (that.y + Ball.HEIGHT/2 > Pong.HEIGHT || that.y - Ball.HEIGHT/2 < 0) {
-			// Goes out of bound! Lose point and restart.
-			that.x = Pong.WIDTH/2;
-			that.y = Pong.HEIGHT/2;
-			vx = 0;
-			vy = 0;
-			moving = false;
-		} else if (that.y - Ball.HEIGHT/2 < Paddle.HEIGHT) {
-			// Chance for ball to collide with top paddle.
-			updateVelocity(topPaddle.x);
-		} else if (that.y + Ball.HEIGHT/2 > Pong.HEIGHT - Paddle.HEIGHT) {
-			// Chance for ball to collide with bottom paddle.
-			updateVelocity(bottomPaddle.x);
-		}
-	}
+        // Check for bouncing
+        if (that.x <= Ball.WIDTH/2 || that.x >= Pong.WIDTH - Ball.WIDTH/2) {
+            // Bounds off horizontally
+            that.vx = -that.vx;
+            that.velocityUpdated = true;
+        } else if (that.y + Ball.HEIGHT/2 > Pong.HEIGHT || that.y - Ball.HEIGHT/2 < 0) {
+            // Goes out of bound! Lose point and restart.
+            that.x = Pong.WIDTH/2;
+            that.y = Pong.HEIGHT/2;
+            that.vx = 0;
+            that.vy = 0;
+            moving = false;
+            that.velocityUpdated = true;
+        } else if (that.y - Ball.HEIGHT/2 < Paddle.HEIGHT) {
+            // Chance for ball to collide with top paddle.
+            updateVelocity(topPaddle.x);
+        } else if (that.y + Ball.HEIGHT/2 > Pong.HEIGHT - Paddle.HEIGHT) {
+            // Chance for ball to collide with bottom paddle.
+            updateVelocity(bottomPaddle.x);
+        }
+    }
 
-	// the following snippet defines an appropriate high resolution 
-	// getTimestamp function depends on platform.
-	if (typeof window === "undefined") {
-		console.log("using process.hrtime()");
-		var getTimestamp = function() { var t = process.hrtime(); return t[0]*1e3 + t[1]*1.0/1e6} 
-	} else if (window.performance !== undefined) {
+    // the following snippet defines an appropriate high resolution 
+    // getTimestamp function depends on platform.
+    if (typeof window === "undefined") {
+        console.log("using process.hrtime()");
+        var getTimestamp = function() { var t = process.hrtime(); return t[0]*1e3 + t[1]*1.0/1e6} 
+    } else if (window.performance !== undefined) {
         if (window.performance.now) {
             console.log("using window.performence.now()");
             var getTimestamp = function() { return window.performance.now(); };
@@ -131,10 +138,10 @@ function Ball() {
             console.log("using window.performence.webkitNow()");
             var getTimestamp = function() { return window.performance.webkitNow(); };
         }
-	} else {
-		console.log("using Date.now();");
-		var getTimestamp = function() { return new Date().now(); };
-	}
+    } else {
+        console.log("using Date.now();");
+        var getTimestamp = function() { return new Date().now(); };
+    }
 }
 
 // Static variables
